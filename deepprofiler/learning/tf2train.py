ac8df5763cb320438386549b78377e50d98f3d42
Nikita Moshkov <nmoshkov@broadinstitute.org>
2021-08-23 07:34:38 -0400

    Training strategy with TF2 dataset and style

--- deepprofiler/learning/tf2train.py
+++ deepprofiler/learning/tf2train.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow_addons as tfa
-
-tf.config.run_functions_eagerly(True)
 import random
+tf.compat.v1.enable_v2_behavior()
+tf.config.run_functions_eagerly(True)
 
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def make_dataset(path, batch_size, single_cell_metadata, labels, is_training):
+def make_dataset(path, batch_size, single_cell_metadata, config, is_training):
     @tf.function
     def fold_channels(crop):
         assert tf.executing_eagerly()
@@ -32,34 +32,38 @@ def make_dataset(path, batch_size, single_cell_metadata, labels, is_training):
 
     def configure_for_performance(ds, is_training):
         ds = ds.shuffle(buffer_size=20000)
-        if is_training:
-            ds = augment(ds)
+        #if is_training:
+        #    ds = augment(ds)
 
         ds = ds.batch(batch_size)
         ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
         return ds
 
     def random_illumination(image):
+        if tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32), tf.cast(0.5, tf.float32)):
         # Make channels independent images
-        numchn = image.shape[-1]
-        source = tf.transpose(image, [2, 1, 0])
-        source = tf.expand_dims(source, -1)
-        source = tf.image.grayscale_to_rgb(source)
-
-        # Apply illumination augmentations
-        bright = tf.random.uniform([numchn], minval=-0.2, maxval=0.2, dtype=tf.float32)
-        channels = [tf.image.adjust_brightness(source[s, ...], bright[s]) for s in range(numchn)]
-        contrast = tf.random.uniform([numchn], minval=0.5, maxval=1.5, dtype=tf.float32)
-        channels = [tf.image.adjust_contrast(channels[s], contrast[s]) for s in range(numchn)]
-        result = tf.concat([tf.expand_dims(t, 0) for t in channels], axis=0)
-
-        # Recover multi-channel image
-        result = tf.image.rgb_to_grayscale(result)
-        result = tf.transpose(result[:, :, :, 0], [2, 1, 0])
-        return result
+            numchn = len(config["dataset"]["images"]["channels"])
+            source = tf.transpose(image, [2, 1, 0])
+            source = tf.expand_dims(source, -1)
+            source = tf.image.grayscale_to_rgb(source)
+        
+            # Apply illumination augmentations
+            bright = tf.random.uniform([numchn], minval=-0.2, maxval=0.2, dtype=tf.float32)
+            channels = [tf.image.adjust_brightness(source[s,...], bright[s]) for s in range(numchn)]
+            contrast = tf.random.uniform([numchn], minval=0.5, maxval=1.5, dtype=tf.float32)
+            channels = [tf.image.adjust_contrast(channels[s], contrast[s]) for s in range(numchn)]
+            result = tf.concat([tf.expand_dims(t, 0) for t in channels], axis=0)
+    
+            # Recover multi-channel image
+            result = tf.image.rgb_to_grayscale(result)
+            result = tf.transpose(result[:,:,:,0], [2, 1, 0])
+        
+            return result
+        else:
+            return image
 
     def random_crop_or_rotate(image):
-        w, h, c = image.shape
+        w, h, c = config["dataset"]["locations"]["box_size"], config["dataset"]["locations"]["box_size"], len(config["dataset"]["images"]["channels"])
         if tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32), tf.cast(0.5, tf.float32)):
             size = tf.random.uniform([1], minval=int(w * 0.6), maxval=w, dtype=tf.int32)
             image = tf.image.random_crop(image, [size[0], size[0], c])
@@ -91,6 +95,7 @@ def make_dataset(path, batch_size, single_cell_metadata, labels, is_training):
     # print(labels)
     filenames_ds = tf.data.Dataset.from_tensor_slices(filenames)
     images_ds = filenames_ds.map(parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    labels = tf.keras.utils.to_categorical(single_cell_metadata["Categorical"])
     labels_ds = tf.data.Dataset.from_tensor_slices(labels)
     ds = tf.data.Dataset.zip((images_ds, labels_ds))
     ds = configure_for_performance(ds, is_training)
@@ -98,11 +103,13 @@ def make_dataset(path, batch_size, single_cell_metadata, labels, is_training):
 
 
 def setup_callbacks(config, strategy_lr):
-
+    callbacks = []
+    
     # CSV Log
     csv_output = config["paths"]["logs"] + "/log.csv"
     callback_csv = tf.keras.callbacks.CSVLogger(filename=csv_output)
-
+    callbacks.append(callback_csv)
+    
     # Checkpoints
     output_file = config["paths"]["checkpoints"] + "/checkpoint_{epoch:04d}.hdf5"
     period = 1
@@ -119,27 +126,33 @@ def setup_callbacks(config, strategy_lr):
         save_best_only=save_best,
         period=period
     )
-
-    def cosine():
-        epochs = config["train"]["model"]["epochs"]
-        lr_schedule_epochs = [x for x in range(epochs)]
-        init_lr = strategy_lr
-        # Linear warm up
-        lr_schedule_lr = [init_lr / (5 - t) for t in range(5)]
-        # Cosine decay
-        lr_schedule_lr += [0.5 * (1 + np.cos((np.pi * t) / epochs)) * init_lr for t in range(5, epochs)]
-        return lr_schedule_lr, lr_schedule_epochs
-
-    if "lr_schedule" in config["train"]["model"] and config["train"]["model"]["lr_schedule"] == "cosine":
-        lr_schedule_lr, lr_schedule_epochs = cosine()
-
-    def lr_schedule(epoch, lr):
-        if epoch in lr_schedule_epochs:
-            return lr_schedule_lr[lr_schedule_epochs.index(epoch)]
+    callbacks.append(callback_model_checkpoint)
+    
+    if "lr_schedule" in config["train"]["model"]:
+        if config["train"]["model"]["lr_schedule"] == "cosine":
+            lr_schedule_epochs = [x for x in range(epochs)]
+            init_lr = config["train"]["model"]["params"]["learning_rate"]
+            # Linear warm up
+            lr_schedule_lr = [init_lr/(5-t) for t in range(5)]
+            # Cosine decay
+            lr_schedule_lr += [0.5 * (1 + np.cos((np.pi * t) / epochs)) * init_lr for t in range(5, epochs)]
         else:
-            return lr
+            assert len(config["train"]["model"]["lr_schedule"]["epoch"]) == \
+                   len(config["train"]["model"]["lr_schedule"]["lr"]), "Make sure that the length of " \
+                                                                        "lr_schedule->epoch equals the length of " \
+                                                                        "lr_schedule->lr in the config file."
 
-    return [callback_model_checkpoint, callback_csv, tf.keras.callbacks.LearningRateScheduler(lr_schedule, verbose=1)]
+            lr_schedule_epochs = config["train"]["model"]["lr_schedule"]["epoch"]
+            lr_schedule_lr = config["train"]["model"]["lr_schedule"]["lr"]
+
+        def lr_schedule(epoch, lr):
+            if lr_schedule_epochs and epoch in lr_schedule_epochs:
+                return lr_schedule_lr[lr_schedule_epochs.index(epoch)]
+            else:
+                return lr
+        callbacks.append(tf.keras.callbacks.LearningRateScheduler(lr_schedule, verbose=1))
+
+    return callbacks
 
 
 def setup_comet_ml(config):
@@ -165,6 +178,7 @@ def learn_model(config, epoch):
     devices = tf.config.experimental.list_physical_devices(
         device_type)
     devices_names = [d.name.split("e:")[1] for d in devices]
+    print(devices_names)
     strategy = tf.distribute.MirroredStrategy(devices=devices_names)
 
     BATCH_SIZE_PER_REPLICA = config["train"]["model"]["params"]["batch_size"]
@@ -174,19 +188,19 @@ def learn_model(config, epoch):
     experiment = setup_comet_ml(config)
     single_cell_metadata = pd.read_csv(os.path.join(config["paths"]["single_cell_sample"], "expanded_sc_metadata.csv"))
     single_cell_metadata = single_cell_metadata[["Class_Name", "Image_Name", "Training_Status"]]
-    labels = tf.keras.utils.to_categorical(single_cell_metadata["Class_Name"])
+    
     num_classes = len(pd.unique(single_cell_metadata["Class_Name"]))
-    single_cell_metadata["Categorical"] = labels
+    single_cell_metadata["Categorical"] = pd.Categorical(single_cell_metadata["Class_Name"]).codes
 
     path = config["paths"]["single_cell_sample"]
     dataset, steps_per_epoch = make_dataset(path, BATCH_SIZE,
                                             single_cell_metadata[single_cell_metadata["Training_Status"] == "SingleCellTraining"],
-                                            labels,
+                                            config,
                                             is_training=True)
     validation_dataset, _ = make_dataset(path, BATCH_SIZE,
-                                        single_cell_metadata[single_cell_metadata["Training_Status"] == "SingleCellValidation"],
-                                        labels,
-                                        is_training=False)
+                                         single_cell_metadata[single_cell_metadata["Training_Status"] == "SingleCellValidation"],
+                                         config,
+                                         is_training=False)
 
     with strategy.scope():
         input_shape = (config["dataset"]["locations"]["box_size"], config["dataset"]["locations"]["box_size"],
@@ -227,7 +241,6 @@ def learn_model(config, epoch):
 
         # => Replicate filters of first layer as needed
         weights = base_model.layers[4].get_weights()
-        print(weights[0].shape)
         available_channels = weights[0].shape[2]
         target_shape = model.layers[4].weights[0].shape
         new_weights = np.zeros(target_shape)
